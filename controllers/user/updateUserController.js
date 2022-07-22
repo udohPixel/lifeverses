cda3f05531aef68aec85a4b6f06420831d951cbd
Udoh Ndem <udohndem@gmail.com>
2022-07-22 08:50:24 +0100

    Sixth Commit - Check for Existence of Email and Password in User Update

--- controllers/user/updateUserController.js
+++ controllers/user/updateUserController.js
@@ -54,6 +54,32 @@ const updateUserController = async (req, res) => {
       });
     }
 
+    // fetch user by username except current user from dB
+    let isExistingUsername = await User.findOne({ username: username })
+      .where("_id")
+      .ne(req.user.id)
+      .exec();
+
+    // fetch user by email except current user from dB
+    let isExistingEmail = await User.findOne({ email: email })
+      .where("_id")
+      .ne(req.user.id)
+      .exec();
+
+    // check if username already exists
+    if (isExistingUsername) {
+      return res.status(409).json({
+        UsernameExistsError: "Username has already been taken. Try another",
+      });
+    }
+
+    // check if username already exists
+    if (isExistingEmail) {
+      return res.status(409).json({
+        EmailExistsError: "Email has already been taken. Try another",
+      });
+    }
+
     // update user
     user = await User.findOneAndUpdate(
       { _id: req.params.id },
